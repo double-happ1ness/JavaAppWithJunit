0754e5b1c1337ba16a00d8d4a862fa0a17a4fc74
double-happ1ness <double.happiness@outlook.com>
2022-06-12 13:01:36 +0100

    first commit

---
+++ src/javaappwithjunit/Truck.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaappwithjunit;
+
+/**
+ *
+ * @author Steve
+ */
+public class Truck {
+    public void run (int miles){
+        
+    }
+    
+}
